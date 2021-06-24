43b86b0084351440f556a83ea3a9208a742f087a
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-25 03:52:32 +0700

    Initial commit

---
+++ src/main/java/com/nyash/rooms/reservationbusinessservices/ReservationBusinessServicesApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.rooms.reservationbusinessservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationBusinessServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationBusinessServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/rooms/reservationbusinessservices/ReservationBusinessServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.rooms.reservationbusinessservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationBusinessServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
