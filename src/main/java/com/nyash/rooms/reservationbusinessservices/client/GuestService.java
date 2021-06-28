2ecfc5fa141a039040ff4448445f712e67b7ddd9
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-28 08:40:57 +0700

    add changes

--- src/main/java/com/nyash/rooms/reservationbusinessservices/client/GuestService.java
+++ src/main/java/com/nyash/rooms/reservationbusinessservices/client/GuestService.java
@@ -1,11 +1,19 @@
 package com.nyash.rooms.reservationbusinessservices.client;
 
+import com.nyash.rooms.reservationbusinessservices.domain.Guest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient("GUESTSERVICES")
 public interface GuestService {
     @RequestMapping(value = "/guests", method = RequestMethod.GET)
-    List<Guest>
+    List<Guest> findAll(@RequestParam(name = "emailAddress", required = false)String emailAddress);
+
+    @RequestMapping(value = "/guests/{id}", method = RequestMethod.GET)
+    Guest findOne(@PathVariable(name = "id")long id);
 }
--- src/main/java/com/nyash/rooms/reservationbusinessservices/client/RoomService.java
+++ src/main/java/com/nyash/rooms/reservationbusinessservices/client/RoomService.java
@@ -2,6 +2,7 @@ package com.nyash.rooms.reservationbusinessservices.client;
 
 import com.nyash.rooms.reservationbusinessservices.domain.Room;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,4 +14,7 @@ public interface RoomService {
 
     @RequestMapping(value = "/rooms", method = RequestMethod.GET)
     List<Room> findAll(@RequestParam(name = "roomNumber", required = false) String roomNumber);
+
+    @RequestMapping(value = "/rooms/{id}", method = RequestMethod.GET)
+    Room findOne(@PathVariable("id") long id);
 }
